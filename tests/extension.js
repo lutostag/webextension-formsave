48a78e72cebd1c6f3d2e9de428f621d7280fee58
Greg Lutostanski <gregory.lutostanski@canonical.com>
2017-03-23 18:25:34 -0500

    fix: re-lint everything
    Make tests and even config-files linted by standard.

---
+++ tests/extension.js
@@ -0,0 +1,10 @@
+/* eslint-env mocha */
+
+const assert = require('assert')
+
+describe('extension', function () {
+  it('moz-extension url reachable', function () {
+    browser.url('moz-extension://88de8cc8-d1ce-484a-bd9f-dd0b100cf262/icons/icon.svg')
+    assert(browser.element('svg').value !== null)
+  })
+})
--- tests/thing.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert')
-
-describe('extension tests', function(){
-    it('moz-extension url reachable',function() {
-        browser.url('moz-extension://88de8cc8-d1ce-484a-bd9f-dd0b100cf262/icons/icon.svg')
-        assert(browser.element('svg').value !== null)
-    })
-})
