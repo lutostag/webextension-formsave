7937d8ea5d0280bc42bb3b7743cb12174c602bda
Greg Lutostanski <gregory.lutostanski@canonical.com>
2017-04-02 17:34:24 -0500

    test: for saving forms

--- tests/extension.js
+++ tests/extension.js
@@ -1,10 +1,19 @@
 /* eslint-env mocha */
 
 const assert = require('assert')
+const prefix = 'moz-extension://88de8cc8-d1ce-484a-bd9f-dd0b100cf262/'
 
 describe('formsave', function () {
   it('has a reachable moz-extension', function () {
-    browser.url('moz-extension://88de8cc8-d1ce-484a-bd9f-dd0b100cf262/icons/icon.svg')
+    browser.url(prefix + 'icons/icon.svg')
     assert(browser.element('svg').value !== null)
+  })
+  it('can save forms', function () {
+    browser.url('http://www.yamllint.com/')
+    browser.setValue('#yaml', 'something to say')
+    browser.pause(300)
+    browser.url(prefix + 'popup/formsave.html')
+    browser.click('.clip')
+    assert(browser.getValue('textarea') === 'something to say')
   })
 })
