0993b50792bcc210c0fe7926c93b586be0438c91
Greg Lutostanski <gregory.lutostanski@canonical.com>
2017-04-16 20:48:40 -0500

    fix: add more tests for searching and sorting

--- tests/extension.js
+++ tests/extension.js
@@ -1,20 +1,47 @@
 /* eslint-env mocha */
 
 const assert = require('assert')
-const prefix = 'moz-extension://88de8cc8-d1ce-484a-bd9f-dd0b100cf262/'
+const extensionPrefix = 'moz-extension://88de8cc8-d1ce-484a-bd9f-dd0b100cf262/'
+const assetsPrefix = 'file://' + process.cwd() + '/tests/assets/'
 
 describe('formsave', function () {
   it('has a reachable moz-extension', function () {
-    browser.url(prefix + 'icons/icon.svg')
+    browser.url(extensionPrefix + 'icons/icon.svg')
     assert(browser.element('svg').value !== null)
   })
-  it('can save forms', function () {
-    browser.url('file://' + process.cwd() + '/tests/assets/textarea.html')
-    browser.setValue('#textarea', 'something to say')
-    browser.pause(500)
-    browser.url(prefix + 'popup/formsave.html')
+  it('saves forms', function () {
+    browser.url(assetsPrefix + 'textarea.html')
+    browser.setValue('#textarea', 'foo')
     browser.pause(300)
+    browser.url(extensionPrefix + 'popup/formsave.html')
     browser.click('.clip')
-    assert(browser.getValue('textarea') === 'something to say')
+    assert(browser.getValue('textarea') === 'foo')
+  })
+  describe('popup', function () {
+    it('searches', function () {
+      browser.url(extensionPrefix + 'popup/formsave.html')
+      browser.setValue('#search', 'foo')
+      browser.pause(300)
+      browser.click('.clip')
+      assert(browser.element('.clip').value !== null)
+      browser.setValue('#search', 'something')
+      browser.pause(300)
+      assert(browser.element('.clip').value === null)
+    })
+    it('sorts', function () {
+      browser.url(assetsPrefix + 'textarea2.html')
+      browser.setValue('#textarea', 'bar')
+      browser.pause(300)
+      browser.url(extensionPrefix + 'popup/formsave.html')
+      browser.click('.clip')
+      browser.click('#url')
+      let element = browser.element('#selection-table > div').value.ELEMENT
+      assert(browser.elementIdAttribute(element, 'class').value.includes('selected'))
+    })
+    it('can reverse sort', function () {
+      browser.click('#url')
+      let element = browser.element('#selection-table > div').value.ELEMENT
+      assert(!browser.elementIdAttribute(element, 'class').value.includes('selected'))
+    })
   })
 })
